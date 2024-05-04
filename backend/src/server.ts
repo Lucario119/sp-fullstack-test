d1522dd63cc983db9e12aef9260d08e1e83c3877
LucasOliveira119 <lucasoliveira@proccedure.com>
2024-05-04 17:21:36 -0400

    port render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import { connectDB } from './database/db';
 import { searchRoute, uploadRoute } from './routes';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 connectDB();
 app.use(cors());
